963f20f8eb5a2fc85de67e48ef55c297d2d03012
dawidziobrowski.dz@gmail.com <dawid.ziobrowski@wp.pl>
2023-01-21 17:17:21 +0100

    Added base functionality

--- sensors/urls.py
+++ sensors/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index),
-    path('add_sensor_log',views.add_sensor_log)
+    path('add_sensor_log', views.add_sensor_log),
+    path('fetch_sensor_logs', views.fetch_sensor_logs)
 ]
